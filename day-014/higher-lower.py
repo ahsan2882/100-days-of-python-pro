57006c837c6f19467d1c17cb6ef06557f8c5a271
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-11-27 20:21:50 +0500

    day 14

--- day-014/higher-lower.py
+++ day-014/higher-lower.py
@@ -8,16 +8,17 @@ def clear():
 
 isGameOver = False
 score = 0
-
+choiceA = choices(data)[0]
+choiceB = choices(data)[0]
 while not isGameOver:
     print(logo)
-    choiceA = choices(data)[0]
-    choiceB = choices(data)[0]
     if choiceA["name"] != choiceB["name"]:
         if choiceA['follower_count'] > choiceB['follower_count']:
             correct = "A"
+            higher_count = choiceA
         else:
             correct = "B"
+            higher_count = choiceB
         print(
             f"Compare A: {choiceA['name']}, {choiceA['description']}, from {choiceA['country']}")
         print(vs)
@@ -26,7 +27,12 @@ while not isGameOver:
         guess = input("Who has more followers, type 'A' or 'B': ")
         if guess.lower() == correct.lower():
             score += 1
+            choiceA = higher_count
+            choiceB = choices(data)[0]
             clear()
         else:
             isGameOver = True
             print(f"You got a score of {score}")
+    else:
+        clear()
+        choiceB = choices(data)[0]
