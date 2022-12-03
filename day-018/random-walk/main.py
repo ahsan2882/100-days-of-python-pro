ed2120fce80737d09f4f0e28ec3fe644bbcecd5d
ahsan2882 <ahsanshahid2882@gmail.com>
2022-12-03 14:03:48 +0500

    day 18

--- day-018/random-walk/main.py
+++ day-018/random-walk/main.py
@@ -1,12 +1,26 @@
 from turtle import Turtle, Screen
 from random import choice, randint
 
+lim = 360
+
 
 def generate_random_color():
     red = randint(0, 255)
     green = randint(0, 255)
     blue = randint(0, 255)
     return (red, green, blue)
+
+
+def draw_boundary():
+    t.pu()
+    t.setpos(-lim, lim)
+    t.pd()
+    for _ in range(4):
+        t.forward(lim*2)
+        t.right(90)
+    t.pu()
+    t.setpos(0, 0)
+    t.pd()
 
 
 t = Turtle()
@@ -18,9 +32,11 @@ screen = Screen()
 screen.colormode(255)
 prev_dir = None
 
+draw_boundary()
+
 for i in range(5000):
-    if thickness < 10:
-        thickness += 0.2
+    if thickness < 15:
+        thickness += 0.02
     t.width(thickness)
     t.color(generate_random_color())
     while True:
@@ -32,12 +48,12 @@ for i in range(5000):
             prev_dir = new_dir
     prev_dir = new_dir
     t_pos = t.pos()
-    if (t_pos[0] > 300 or t_pos[0] < -300 or
-            t_pos[1] > 300 or t_pos[1] < -300):
-        t.pu()
-        t.setpos(0, 0)
-        t.pd()
-    t.right(new_dir)
-    t.forward(20)
+    if (t_pos[0] > (lim-20) or t_pos[0] < -(lim-20) or
+            t_pos[1] > (lim-20) or t_pos[1] < -(lim-20)):
+        t.left(90)
+        t.forward(20)
+    else:
+        t.right(new_dir)
+        t.forward(20)
 
 screen.exitonclick()
