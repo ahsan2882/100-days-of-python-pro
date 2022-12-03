72383c7c0cb4e521693778d815a52069185c0403
ahsan2882 <ahsanshahid2882@gmail.com>
2022-12-03 10:31:38 +0500

    day 17 quiz game OOP

---
+++ day-017/quiz-project/data.py
@@ -0,0 +1,202 @@
+question_data = [
+    {
+        "question": "In the &quot;Shrek&quot; film franchise, Donkey is played by Eddie Murphy.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The Ceratosaurus, a dinosaur known for having a horn on the top of its nose, is also known to be a decendent of the Tyrannosaurus Rex.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Seoul is the capital of North Korea.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Greenland is covered with grass and Iceland covered with ice.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Vatican City is a country.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In the play Oedipus Rex, Oedipus kills his father due to jealousy in loving his mother.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "There was a satirical candidate named &quot;Deez Nuts&quot; running in the 2016 US presidential elections.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The word &quot;Inception&quot; came from the 2010 blockbuster hit &quot;Inception&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The &#039;Squaring the Circle&#039; problem is solvable.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The game &quot;Battlefield 1&quot; takes place during World War I.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The binary number &quot;101001101&quot; is equivalent to the Decimal number &quot;334&quot;",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Ottawa is the capital of Canada.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "MacOS is based on Linux.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In &quot;Team Fortress 2&quot;, the &quot;Bill&#039;s Hat&quot; is a reference to the game &quot;Dota 2&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The Sun rises from the North.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The color orange is named after the fruit.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The term &quot;Spam&quot; came before the food product &quot;Spam&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The French word to travel is &quot;Travail&quot;",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The Western Electric Model 500 telephone uses tone dialing to dial phone numbers.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The main playable character of the 2015 RPG &quot;Undertale&quot; is a monster.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The French word for &quot;glass&quot; is &quot;glace&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Time on Computers is measured via the EPOX System.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In the movie The Revenant, DiCaprio&#039;s character hunts down the killer of his son.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The Hundred Years&#039; War was fought for more than a hundred years.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "E = MC3",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Harry Potter was born on July 31st, 1980.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In Pok&eacute;mon, Arbok evolves into Seviper.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Nickelodeon rejected the pilot to Adventure Time.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The State of Queensland, Australia voted NO to a referendum for daylight savings in 1992.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Klingons express emotion in art through opera and poetry.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In &quot;Star Trek&quot;, Klingons are commonly referred to as &quot;Black Elves&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The set of all algebraic numbers is countable.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Actor Tommy Chong served prison time.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In &quot;To Love-Ru: Darkness&quot;, Yami reveals her real name is Eve.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In &quot;League of Legends&quot;, there exists four different types of Dragon.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The scrapped Sonic the Hedgehog 2 level &quot;Hidden Palace Zone&quot; was later reused in the iOS port of the game. ",
+        "correct_answer": "True",
+    },
+    {
+        "question": "&quot;Green Eggs and Ham&quot; consists of only 50 different words.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The title of the 1969 film &quot;Krakatoa, East_of Java&quot; is incorrect, as Krakatoa is in fact west of Java.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "During pre-production of &quot;Super Mario Bros.&quot;, Mario originally was meant to shoot bullets.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Shang Tsung is a playable character in Mortal Kombat XL.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The protagonist&#039;s names in &#039;Who&#039;s Afraid of Virginia Woolf&#039;, George and Martha, were derived from George Washington and his wife.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Shub-Niggurath is a creature that was created by \tJ. R. R. Tolkien in his novel &quot;The Lord of The Rings&quot;.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Jingle Bells was originally meant for Thanksgiving",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Ping-Pong originated in England",
+        "correct_answer": "True",
+    }
+]
