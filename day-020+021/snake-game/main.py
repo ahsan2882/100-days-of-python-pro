b1723a3b92854de754989031a887b035bc757400
ahsan2882 <ahsanshahid2882@gmail.com>
2022-12-04 13:59:41 +0500

    add difficulty in game

--- day-020+021/snake-game/main.py
+++ day-020+021/snake-game/main.py
@@ -1,16 +1,18 @@
 from turtle import Screen
 from snake import Snake
+from difficulty import Difficulty
 
 
 screen = Screen()
-snake_player = Snake(screen)
-# screen = GameScreen(snake_player.up, snake_player.down,
-#                     snake_player.left, snake_player.right)
+screen.tracer(0)
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+user_diff = screen.textinput(
+    title="Choose difficulty", prompt="Easy, Medium, Hard")
+print(user_diff)
+snake_player = Snake(screen=screen, difficulty=user_diff)
 screen.listen()
-screen.tracer(0)
 screen.onkeypress(snake_player.up, "Up")
 screen.onkeypress(snake_player.down, "Down")
 screen.onkeypress(snake_player.left, "Left")
@@ -19,8 +21,14 @@ screen.onkeypress(snake_player.right, "Right")
 screen.update()
 
 game_is_on = True
+speed = 800
+if user_diff.lower() == Difficulty.MEDIUM.value:
+    speed = 500
+elif user_diff.lower() == Difficulty.HARD.value:
+    speed = 200
 
 while game_is_on:
-    screen.ontimer(snake_player.move(), 100)
+    screen.update()
+    screen.ontimer(snake_player.move(), speed)
 
 screen.exitGame()
--- day-020+021/snake-game/snake.py
+++ day-020+021/snake-game/snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from difficulty import Difficulty
 
 UP = 90
 DOWN = 270
@@ -7,16 +8,16 @@ RIGHT = 0
 
 
 class Snake():
-    def __init__(self, screen):
+    def __init__(self, screen, difficulty):
         self.snake_body = []
         self.screen = screen
+        self.difficulty = difficulty
         self.turtle_position = (0, 0)
         self.head = self.create_snake()
 
     def create_snake(self):
         for i in range(8):
             snake = Turtle(shape="square")
-            snake.speed(1)
             if i == 0:
                 snake.color("red")
             else:
@@ -32,7 +33,22 @@ class Snake():
         for seg_num in range(len(self.snake_body)-1, 0, -1):
             self.snake_body[seg_num].goto(self.snake_body[seg_num-1].pos())
         self.head.forward(20)
-        self.screen.update()
+        if self.difficulty.lower() == Difficulty.EASY.value:
+            if self.head.pos()[0] < ((self.screen.window_width()/2) - 30)*-1:
+                self.head.goto(
+                    (self.screen.window_width()/2) - 30,
+                    self.head.pos()[1])
+            elif self.head.pos()[0] > (self.screen.window_width()/2) - 30:
+                self.head.goto(
+                    ((self.screen.window_width()/2) - 30)*-
+                    1, self.head.pos()[1]
+                )
+            elif self.head.pos()[1] < ((self.screen.window_height()/2)-30)*-1:
+                self.head.goto(
+                    self.head.pos()[0], (self.screen.window_height()/2)-30)
+            elif self.head.pos()[1] > ((self.screen.window_height()/2)-30):
+                self.head.goto(
+                    self.head.pos()[0], ((self.screen.window_height()/2)-30)*-1)
 
     def up(self):
         if self.head.heading() != DOWN:
