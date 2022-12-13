702ea505c9d2309d520f83e19627dda04e9037f6
ahsan2882 <ahsanshahid2882@gmail.com>
2022-12-13 11:13:59 +0500

    save high score

--- day-024/scoreboard.py
+++ day-024/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+from os.path import exists, abspath
+import pathlib
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
@@ -7,7 +9,17 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        loc = pathlib.Path(__file__).parent.resolve()
+        self.txt_file_loc = pathlib.Path(loc, "high_score.txt").resolve()
+        if not exists(self.txt_file_loc):
+            file = open(self.txt_file_loc, "w")
+            self.high_score = 0
+            file.write("0")
+        else:
+            file = open(self.txt_file_loc, "r")
+            self.high_score = int(file.read())
+        file.close()
+
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -26,6 +38,8 @@ class Scoreboard(Turtle):
     def game_reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open(self.txt_file_loc, "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
