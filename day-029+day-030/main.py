4013696b16a696558716c50ef9003622071ea74d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2023-01-08 13:49:02 +0500

    search and update data

--- day-029+day-030/main.py
+++ day-029+day-030/main.py
@@ -31,15 +31,10 @@ except FileExistsError:
     pass
 
 
-def getDataFromForm():
+def getDataAndSave():
     website = website_entry.get()
     username = email_entry.get()
     password = password_entry.get()
-    return (website, username, password)
-
-
-def getDataAndSave():
-    website, username, password = getDataFromForm()
     new_data = {
         website: {
             'email': username,
@@ -70,6 +65,68 @@ def getDataAndSave():
             email_entry.delete(0, 'end')
             password_entry.delete(0, 'end')
             website_entry.focus()
+
+
+def search_password():
+    site = website_entry.get()
+    if site == "":
+        messagebox.showerror(
+            title="Oops", message="Please enter the website name to search!")
+    else:
+        with open(FILE_PATH, 'r') as secure_file:
+            secure_file_data = json.load(secure_file)
+            try:
+                for data in secure_file_data:
+                    if site in data:
+                        username = data[site]['email']
+                        password = data[site]['password']
+                        option = messagebox.askyesno(
+                            title=site, message=f"Email: {username} \nPassword: {password}\nDo you want to update this data?")
+                        if option:
+                            email_entry.delete(0, 'end')
+                            email_entry.insert(0, data[site]['email'])
+                            password_entry.delete(0, 'end')
+                            password_entry.insert(0, data[site]['password'])
+                            website_entry.config(state="disabled")
+                            add_entry_button.config(
+                                text="Update", command=updateData)
+                        else:
+                            pyperclip.copy(data[site]['password'])
+                        break
+                    else:
+                        raise KeyError("No details for the website exists!")
+            except KeyError as error:
+                print(error)
+                messagebox.showerror(
+                    title="Oops", message="No details for the website exists!")
+
+
+def updateData():
+    username = email_entry.get()
+    password = password_entry.get()
+    website = website_entry.get()
+
+    if username == "" or password == "":
+        messagebox.showerror(
+            title="Oops", message="Please don't leave any fields empty!")
+    else:
+        with open(FILE_PATH, 'r') as secure_file:
+            secure_file_data = json.load(secure_file)
+            for data in secure_file_data:
+                if website in data:
+                    data[website]['email'] = username
+                    data[website]['password'] = password
+                    break
+        with open(FILE_PATH, 'w') as secure_file:
+            data = json.dumps(secure_file_data, indent=4)
+            secure_file.write(data)
+
+    website_entry.config(state="normal")
+    add_entry_button.config(text="Add", command=getDataAndSave)
+
+    website_entry.delete(0, 'end')
+    email_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -102,7 +159,9 @@ website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 website_entry = Entry(width=50)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2, sticky="EW", pady=2)
+website_entry.grid(column=1, row=1, sticky="EW", pady=2)
+search_button = Button(text="Search", width=15, command=search_password)
+search_button.grid(column=2, row=1, padx=2, sticky="EW")
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
