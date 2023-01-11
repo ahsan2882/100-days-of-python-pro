a8c54a6111476cad8074134bfd76944a79484ccb
ahsan2882 <ahsanshahid2882@gmail.com>
2023-01-11 12:07:12 +0500

    generate new word cards

--- day-031-flashcard-capstone-project/main.py
+++ day-031-flashcard-capstone-project/main.py
@@ -1,10 +1,18 @@
 from tkinter import Tk, Canvas, PhotoImage, Button, Entry, Label
 from pathlib import Path
+import pandas as pd
+from random import choice
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 ITALIC_FONT = ("Arial", 40, "italic")
 BOLD_FONT = ("Arial", 60, "bold")
+
+DATA_FILE_PATH = Path(
+    Path(__file__).parent.resolve(),
+    'data',
+    'french_words.csv'
+).resolve()
 
 CARD_BACK_PATH = Path(
     Path(__file__).parent.resolve(),
@@ -12,26 +20,34 @@ CARD_BACK_PATH = Path(
     'card_back.png'
 ).resolve()
 
-
 CARD_FRONT_PATH = Path(
     Path(__file__).parent.resolve(),
     'images',
     'card_front.png'
 ).resolve()
 
-
-RIGHT_PATH = Path(
+CORRECT_ANSWER_BUTTON_IMAGE_PATH = Path(
     Path(__file__).parent.resolve(),
     'images',
     'right.png'
 ).resolve()
 
-
-WRONG_PATH = Path(
+UNKNOWN_ANSWER_BUTTON_IMAGE_PATH = Path(
     Path(__file__).parent.resolve(),
     'images',
     'wrong.png'
 ).resolve()
+
+df = pd.read_csv(DATA_FILE_PATH)
+words_to_learn = df.to_dict(orient="records")
+
+
+def pick_word():
+    new_word = choice(words_to_learn)
+    new_word_french = new_word["French"]
+    new_word_english = new_word["English"]
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=new_word_french, fill="black")
 
 
 window = Tk()
@@ -40,20 +56,24 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 card_back_photo = PhotoImage(file=CARD_BACK_PATH)
 card_front_photo = PhotoImage(file=CARD_FRONT_PATH)
-right_photo = PhotoImage(file=RIGHT_PATH)
-wrong_photo = PhotoImage(file=WRONG_PATH)
+correct_answer_icon = PhotoImage(file=CORRECT_ANSWER_BUTTON_IMAGE_PATH)
+unknown_answer_icon = PhotoImage(file=UNKNOWN_ANSWER_BUTTON_IMAGE_PATH)
 
 canvas = Canvas(width=800, height=526,
                 bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_photo)
-canvas.create_text(400, 150, text="French", font=ITALIC_FONT)
-canvas.create_text(400, 263, text="Bonjour", font=BOLD_FONT)
+card_title = canvas.create_text(400, 150, text="", font=ITALIC_FONT)
+card_word = canvas.create_text(400, 263, text="", font=BOLD_FONT)
 canvas.grid(column=0, row=0, columnspan=2)
 
-button_right = Button(image=right_photo, highlightthickness=0)
+button_right = Button(image=correct_answer_icon,
+                      highlightthickness=0, command=pick_word)
 button_right.grid(column=1, row=1)
 
-button_wrong = Button(image=wrong_photo, highlightthickness=0)
+button_wrong = Button(image=unknown_answer_icon,
+                      highlightthickness=0, command=pick_word)
 button_wrong.grid(column=0, row=1)
+
+pick_word()
 
 window.mainloop()
