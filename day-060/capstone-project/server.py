c90af310088f3689a75d40a90bc93cba2c88b39f
ahsan2882 <ahsanshahid2882@gmail.com>
2023-02-10 17:00:16 +0500

    day 60 contact page

---
+++ day-060/capstone-project/server.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+
+app = Flask(__name__)
+
+all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post["id"] == post_id:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route('/about')
+def go_to_about():
+    return render_template("about.html")
+
+
+# @app.route('/contact')
+# def go_to_contact():
+#     return render_template("contact.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
