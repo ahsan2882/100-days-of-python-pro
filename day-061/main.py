44bf84618852ffccae4b5ed554f39cb4026829e8
ahsan2882 <ahsanshahid2882@gmail.com>
2023-02-12 11:49:33 +0500

    Using WTForms

---
+++ day-061/main.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from wtforms import StringField
+from flask_wtf import FlaskForm
+from flask import Flask, render_template
+
+DOTENV_PATH = Path(
+    Path(__file__).parent.resolve(), '.env'
+).resolve()
+
+load_dotenv(DOTENV_PATH)
+
+APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
+
+app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
+
+
+app.secret_key = APP_SECRET_KEY
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def user_login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
